abda51aa7defe8f754fcc9e4858fc46bd6d9845f
Chen Zihui <hello@chenzihui.com>
2014-09-14 10:40:14 +0800

    Future months not selectable

--- src/controllers/sidebar_ctrl.js
+++ src/controllers/sidebar_ctrl.js
@@ -1,7 +1,22 @@
 'use strict';
 
 App.SidebarController = Ember.Controller.extend({
-  months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
-    'Oct', 'Nov', 'Dec'
-  ]
+
+  months: Ember.computed(function() {
+    var all  = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+      'August', 'September', 'October', 'November', 'December'],
+
+        curr = new Date().getMonth(),
+        selectable;
+
+    return all.map(function(mth, index) {
+      if (index <= curr) {
+        selectable = true;
+      } else {
+        selectable = false;
+      }
+
+      return { name: mth, selectable: selectable };
+    });
+  })
 });
