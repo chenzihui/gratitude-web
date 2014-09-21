f9c1d78331040ab90c804b0ad66327b9fa35065e
Chen Zihui <hello@chenzihui.com>
2014-09-21 11:48:40 +0800

    Add route to get entries from specific date

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@
 
 App.Router.map(function() {
   this.resource('entries', { path: '/' }, function() {
-    // Nested routes
+    this.route('detail', { path: ':year/:month/:day' });
   });
 });
 
@@ -26,6 +26,35 @@ App.EntriesIndexRoute = Ember.Route.extend({
 
     this._super(controller, model);
     controller.set('currentDate', today);
+  },
+
+  renderTemplate: function(controller) {
+    this.render('entries/index', { controller: controller });
+  }
+});
+
+App.EntriesDetailRoute = Ember.Route.extend({
+  controllerName: 'entries',
+
+  model: function(params) {
+    var date      = new Date(params.year, params.month - 1, params.day),
+        format    = 'YYYY-M-D',
+        formatted = moment(date).format(format),
+
+        model     = this.modelFor('entries');
+
+    model.set('currentDate', moment(date).format('dddd, MMMM Do YYYY'));
+
+    return model.filter(function(entry) {
+      return moment(entry.get('createdAt')).format(format) == formatted
+    });
+  },
+
+  setupController: function(controller, model) {
+    var currentDate = this.modelFor('entries').get('currentDate');
+
+    this._super(controller, model);
+    controller.set('currentDate', currentDate);
   },
 
   renderTemplate: function(controller) {
