6454ed89260ccfcafb50945cc6d388914f73b384
Chen Zihui <hello@chenzihui.com>
2014-09-27 13:27:42 +0800

    Let modal dialog be a route

--- src/router.js
+++ src/router.js
@@ -1,20 +1,22 @@
 'use strict';
 
 App.Router.map(function() {
-  this.resource('entries', { path: '/' }, function() {
-    this.route('detail', { path: ':year/:month/:day' });
+  this.resource('journal', { path: '/' }, function() {
+    this.resource('entries', { path: '/journal/:year/:month/:day' }, function() {
+      this.route('calendar');
+    });
   });
 });
 
-App.EntriesRoute = Ember.Route.extend({
-  model: function(params) {
+App.JournalRoute = Ember.Route.extend({
+  model: function() {
     return this.store.find('entry');
   },
 
   actions: {
     openCalendar: function() {
       return this.render('entries/calendar', {
-        into: 'entries',
+        into: 'journal',
         outlet: 'modal'
       });
     },
@@ -22,20 +24,22 @@ App.EntriesRoute = Ember.Route.extend({
     closeCalendar: function() {
       return this.disconnectOutlet({
         outlet: 'modal',
-        parentView: 'entries'
+        parentView: 'journal'
       });
     }
   }
 });
 
-App.EntriesIndexRoute = Ember.Route.extend({
+App.JournalIndexRoute = Ember.Route.extend({
+  controllerName: 'entriesIndex',
+
   model: function() {
     var format = 'YYYY-M-D',
         today  = moment().format(format),
 
         createdAt;
 
-    return this.modelFor('entries').filter(function(entry) {
+    return this.modelFor('journal').filter(function(entry) {
       createdAt = moment(entry.get('createdAt')).format(format);
       return createdAt == today;
     });
@@ -54,25 +58,27 @@ App.EntriesIndexRoute = Ember.Route.extend({
   }
 });
 
-App.EntriesDetailRoute = Ember.Route.extend({
-  controllerName: 'entriesIndex',
-
+App.EntriesRoute = Ember.Route.extend({
   model: function(params) {
     var date      = new Date(params.year, params.month - 1, params.day),
         format    = 'YYYY-M-D',
         formatted = moment(date).format(format),
 
-        model     = this.modelFor('entries');
-
-    model.set('currentDate', moment(date).format('dddd, MMMM Do YYYY'));
+        model     = this.modelFor('journal');
 
     return model.filter(function(entry) {
       return moment(entry.get('createdAt')).format(format) == formatted
-    });
+    }).set('currentDate', moment(date).format('dddd, MMMM Do YYYY'));
+  }
+});
+
+App.EntriesIndexRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.modelFor('entries');
   },
 
   setupController: function(controller, model) {
-    var currentDate = this.modelFor('entries').get('currentDate');
+    var currentDate = model.get('currentDate');
 
     this._super(controller, model);
     controller.set('currentDate', currentDate);
